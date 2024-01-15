4bdd55c7ea92fe0ac1b04b09ed7d14921ac37856
DrPixels <145904039+DrPixels@users.noreply.github.com>
2024-01-15 15:47:08 +0800

    Add files via upload

---
+++ _1_HelloWorld.java
@@ -0,0 +1,10 @@
+package _1_Week1_IntroductionToJava;
+
+public class _1_HelloWorld {
+	public static void main(String...args) {
+		System.out.println("Hellow World");
+		System.out.println("Name: Abigail Lapuz");
+		System.out.println("Program: BSIT");
+		System.out.println("Section: IT12S1");
+	}
+}
